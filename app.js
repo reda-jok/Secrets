b2fdec4d277e62f323505335fb5f6b0c254c90ee
reda-jok <redajok22m@gmail.com>
2023-09-23 13:16:16 +0300

    a lot of changes : 1- add more security by hashing password and add salts. 2- authenticate the user and add cookies, so when use logged in well be saved

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
-
 const bodyParser = require('body-parser')
 const express = require('express')
 const ejs = require('ejs')
 const { default: mongoose } = require('mongoose')
-const encryption = require('mongoose-encryption')
+const session = require('express-session')
+const passport = require('passport')
+const passportLocalMongoose = require('passport-local-mongoose')
+
 
 const app = express()
 app.use(express.static('public'))
@@ -13,18 +15,31 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
+
+app.use(session({
+    secret:'Our littel secret.',
+    resave: false,
+    saveUninitialized : false,
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
 
 const userSchema = new mongoose.Schema({
     email: String,
     password:String
 })
+userSchema.plugin(passportLocalMongoose)
 
-
-const secret = process.env.SECRET 
-userSchema.plugin(encryption,{secret:secret, encryptedFields:['password']})
 
 const User = new mongoose.model('User', userSchema)
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 
 app.get('/', (req, res)=>{
     res.render('home')
@@ -41,41 +56,55 @@ app.get('/register', (req, res)=>{
 })
 
 
-app.post('/register',(req,res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
+app.get('/logout',(req,res)=>{
+    req.logout((err)=>{
+        if (err){console.log(err)}
     })
+    res.redirect('/')
+})
 
-    newUser.save()
-    .catch(err=>{
-        console.error('error saving user', err.message)
-    })
-    res.render('secrets')
+
+app.get('/secrets',(req,res)=>{
+
+    if(req.isAuthenticated()){
+        res.render('secrets')
+    }else{
+        res.redirect('/login')
+    }
+})
+app.post('/register',(req,res)=>{
+
+   User.register({username: req.body.username}, req.body.password,(err,user)=>{
+    if(err){
+        console.log(err)
+        res.redirect('/register')
+
+    }else{
+        passport.authenticate('local')(req,res,()=>{
+            res.redirect('/secrets')
+        })
+    }
+   })
 })
 
 
 app.post('/login', (req,res)=>{
-    const username =req.body.username
-    const password = req.body.password
-
-    User.findOne({email:username})
-    .then(user => {
-        if (user){
-            if (user.password === password){
-                console.log('correct password')
-                res.render('secrets')
-            }else{
-                console.log('Worng password')
-            }
+    const user = new User({
+        username : req.body.username,
+        password : req.body.password,
+    })
+  
+    req.login(user,(err)=>{
+        if (err){
+            console.log(err)
         }else{
-            console.log('Email dose not exite in the data base')
+            passport.authenticate('local')(req,res,()=>{
+                res.redirect('/secrets')
+            })
         }
     })
-    .catch(err =>{
-        console.error('Error checking email', err)
-    })
 })
+
 
 app.listen(3000, ()=>{
     console.log('server started on port 3000.')
